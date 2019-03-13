ecdd5eb22d46394e7e61418c1e676b2640618a38
Emily Higgins <emilyrchiggins@gmail.com>
2019-03-13 16:32:42 -0400

    dom manipulation

--- WeekA/ControlFlow.js
+++ WeekA/ControlFlow.js
@@ -1,3 +1,21 @@
+//"use strict";
+
+//apply an event listener to Window object 
+//this function will execute when the window loads
+window.onload = function(){
+    //select the element from the DOM with id "compare"
+    //apply an event listener to our Compare button
+    //anonymous function provided will be invoked when the element is clicked
+    document.getElementById("compare").addEventListener("click", function(){
+        //alert("hello from compare button!");
+        //retrieve values from user input on the form
+        let num1 = document.getElementById("num1").value;
+        let num2 = document.getElementById("num2").value;
+        let num3 = document.getElementById("num3").value;
+        console.log(largestOfThree(num1,num2,num3));
+    })    
+}
+
 //incrementing, pre and post
 function funWithIncrementing(num){
     //num++; //post incrementer, increments after evaluating
@@ -32,11 +50,29 @@ function loops(){
     while (arr[y]){
         console.log(arr[y]);
         y++;
-    }
-
-    function largestOfThree(a,b,c){
-        //return the largest parameter
-    }
+    }  
 }
 
 
+function largestOfThree(a,b,c){
+    //return the largest parameter
+    //check whether input is of type number
+    //note: Number(boolean) will evaluate to 0 or 1
+    //use typeof
+    if(isNaN(Number(a)) || isNaN(Number(b)) || isNaN(Number(c))){
+        return NaN;
+    } else {
+        let largest = a;
+        console.log("largest is "+a);
+        if (b > largest) {
+            console.log("largest is "+b);
+            largest = b;
+        }
+        if (c > largest) {
+            console.log("largest is "+c);
+            largest = c;
+        }
+        return largest;
+    }
+
+}
