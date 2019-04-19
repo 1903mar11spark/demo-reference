bedfb3285b95a1738f8426caba31d472b2e189cc
Emily Higgins <emilyrchiggins@gmail.com>
2019-04-19 11:20:35 -0400

    end of login demo

--- ServletLoginApp/src/main/webapp/scripts/check.js
+++ ServletLoginApp/src/main/webapp/scripts/check.js
@@ -1,3 +1,25 @@
-/**
- * 
- */
+let user = {};
+
+window.onload = function() {
+	populateUser();
+}
+
+function populateUser() {
+	//send GET request to SessionServlet to obtain session information
+	fetch("http://localhost:8084/ServletLoginApp/session").then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		//check whether there was a valid session
+		//define behavior for no user returned
+		if (data.session === null) {
+			window.location = "http://localhost:8084/ServletLoginApp/login";
+		} else {
+			//define behavior for user returned
+			user = data;
+			document.getElementById("username").innerText = "username: "+user.username;
+			document.getElementById("firstname").innerText = "firstname: "+user.firstname;
+			document.getElementById("lastname").innerText = "lastname: "+user.lastname;
+			document.getElementById("email").innerText = "email: "+user.email;
+		}
+	});
+}
