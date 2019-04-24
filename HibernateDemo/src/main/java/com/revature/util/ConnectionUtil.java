dd50eb585c8bd54f1b090bb2a1feec6980ae9703
Emily Higgins <emilyrchiggins@gmail.com>
2019-04-24 13:20:20 -0400

    basic setup, part 1

---
+++ HibernateDemo/src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,24 @@
+package com.revature.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ConnectionUtil {
+	
+	private ConnectionUtil() {
+		super();
+	}
+	
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			// if no SessionFactory has been created, create one
+			Configuration c = new Configuration();
+			// grab credentials and url from environment variables
+			c.setProperty("hibernate.connection.username", System.getenv("BEARS_DB_USERNAME"));
+		}
+		return sessionFactory;
+	}
+
+}
