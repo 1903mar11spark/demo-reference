e3908314fce330290602955c6bfb382afa19c497
Emily Higgins <emilyrchiggins@gmail.com>
2019-05-03 13:14:31 -0400

    adding extra Flashcard endpoints

--- SpringMVCDemo/src/main/java/com/revature/controllers/FlashcardController.java
+++ SpringMVCDemo/src/main/java/com/revature/controllers/FlashcardController.java
@@ -6,26 +6,67 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.revature.beans.Flashcard;
 import com.revature.service.FlashcardService;
 
 @Controller
-@RequestMapping(value="/flashcard")
+@ResponseBody // indicates that responses are NOT view names (skipping view resolver)
+@RequestMapping(value = "/flashcard")
 public class FlashcardController {
-	
+
 	private FlashcardService flashcardService;
-	
+
 	@Autowired // setter injection
 	public void setFlashcardService(FlashcardService flashcardService) {
 		this.flashcardService = flashcardService;
 	}
-	
-	@RequestMapping(value="/all", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public ResponseEntity<List<Flashcard>> getAllFlashcards() {
 		return new ResponseEntity<>(flashcardService.getAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Flashcard> getFlashcardById(@PathVariable int id){
+		Flashcard f = flashcardService.getById(id);
+		if (f == null) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<>(f, HttpStatus.OK);
+		}
+	}
+	
+	// rewrite the above to use a query string 
+	// (will also work for form data passed in request body)
+	@GetMapping(value="/byId")
+	public ResponseEntity<Flashcard> getFlashcardByIdWithQueryString(@RequestParam int id){
+		Flashcard f = flashcardService.getById(id);
+		if (f == null) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<>(f, HttpStatus.OK);
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> addFlashcard(@RequestBody Flashcard flashcard) {
+		ResponseEntity<String> resp = null;
+		try {
+			flashcardService.create(flashcard);
+			resp = new ResponseEntity<>("FLASHCARD CREATED SUCCESSFULLY", HttpStatus.OK);
+		} catch (Exception e) {
+			resp = new ResponseEntity<>("FAILED TO CREATE FLASHCARD", HttpStatus.BAD_REQUEST);
+		}
+		return resp;
 	}
 
 }
--- SpringMVCDemo/src/main/java/com/revature/service/FlashcardService.java
+++ SpringMVCDemo/src/main/java/com/revature/service/FlashcardService.java
@@ -12,14 +12,35 @@ import com.revature.beans.FlashcardTopics;
 @Service
 public class FlashcardService {
 
-	public List<Flashcard> getAll() {
-		Author a = new Author("Simon", "Garfunkel");
+	private Author a = new Author("Simon", "Garfunkel");
+	private List<Flashcard> fl = new ArrayList<>();
+
+	{
 		Flashcard f1 = new Flashcard(4, "What is the answer?", "42", FlashcardTopics.MATH, a);
 		Flashcard f2 = new Flashcard(5, "What is the question?", "what is six times eight?", FlashcardTopics.MATH, a);
-		List<Flashcard> fl = new ArrayList<>();
+
 		fl.add(f1);
 		fl.add(f2);
+	}
+
+	public List<Flashcard> getAll() {
+
 		return fl;
+	}
+
+	public Flashcard getById(int id) {
+		Flashcard card = null;
+		for (Flashcard f : fl) {
+			if (f.getId() == id) {
+				card = f;
+				break;
+			}
+		}
+		return card;
+	}
+	
+	public void create(Flashcard flashcard) {
+		fl.add(flashcard);
 	}
 
 }
